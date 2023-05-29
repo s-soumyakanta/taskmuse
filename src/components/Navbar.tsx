ca26a5150f26f52e72a87418cf67fc52000f3388
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2023-05-29 23:58:06 +0530

    Navbar basic layout and test completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,47 @@
-
+import menu from "../assets/hambuger.svg"
+import search from "../assets/search.svg"
+import view from "../assets/grid.svg"
+import sort from "../assets/filter.svg"
+import about from "../assets/info.svg"
+import download from "../assets/download.svg"
+import logo from "../assets/logo.png"
 
 const Nav = () => {
   return (
     <>
-     <nav className="bg-primary-400  text-white">
-       Navbar
+     <nav className="flex flex-row w-full mt-2 border-b-2 ">
+       <div className="flex flex-row w-1/6 justify-between px-8 items-center">
+        <div className="w-8">
+          <img src={menu} alt="menu" />
+        </div>
+        <div className="w-16">
+          <img src={logo} alt="taskzen" />
+        </div>
+       </div>
+
+       <div className="flex  w-5/6 justify-between items-center px-8 ">
+        <div className="w-4/6">
+        <div className=" flex justify-end ">
+          <img src={search} alt="search" className="w-6" />
+        </div>
+        </div>
+        <div className="w-2/6 flex justify-evenly items-center">
+
+      
+        <div className="w-6">
+          <img src={view} alt="view" />
+        </div>
+        <div className="w-6">
+          <img src={sort} alt="sort" />
+        </div>
+        <div className="w-6">
+          <img src={about} alt="about" />
+        </div>
+        <div className="w-6">
+          <img src={download} alt="download" />
+        </div>
+        </div>
+       </div>
        
      </nav>
       </>
--- src/components/__tests__/navbar.test.tsx
+++ src/components/__tests__/navbar.test.tsx
@@ -1,31 +1,49 @@
-import { screen,render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import Navbar from "../Navbar";
 
+describe("Navbar rendering svg icons correctly", () => {
+  test("SVG icons rendering correctly", () => {
+    render(<Navbar />);
 
-describe("Navbar rendering svg icons correctly",()=>{
-    test("SVG icons rendering correctly",()=>{
-        render(<Navbar />)
+    const svgIconNames:string[] = ["menu", "taskzen", "search", "view", "sort", "about", "download"];
 
-        const menu = screen.getByAltText("menu")
-        expect(menu).toBeInTheDocument()
+    svgIconNames.forEach((iconName:string) => {
+      const icon:HTMLElement = screen.getByAltText(iconName);
+      expect(icon).toBeInTheDocument();
+    });
+  });
+});
 
-        const logo = screen.getByAltText("taskzen")
-        expect(logo).toBeInTheDocument()
 
-        const search = screen.getByAltText("search")
-        expect(search).toBeInTheDocument()
 
-        const view = screen.getByAltText("view")
-        expect(view).toBeInTheDocument()
 
-        const sort = screen.getByAltText("sort")
-        expect(sort).toBeInTheDocument()
 
-        const about = screen.getByAltText("about")
-        expect(about).toBeInTheDocument()
 
-        const download = screen.getByAltText("download")
-        expect(download).toBeInTheDocument()
 
-    })
-})
+
+
+// test("SVG icons rendering correctly",()=>{
+//     render(<Navbar />)
+
+//     const menu = screen.getByAltText("menu")
+//     expect(menu).toBeInTheDocument()
+
+//     const logo = screen.getByAltText("taskzen")
+//     expect(logo).toBeInTheDocument()
+
+//     const search = screen.getByAltText("search")
+//     expect(search).toBeInTheDocument()
+
+//     const view = screen.getByAltText("view")
+//     expect(view).toBeInTheDocument()
+
+//     const sort = screen.getByAltText("sort")
+//     expect(sort).toBeInTheDocument()
+
+//     const about = screen.getByAltText("about")
+//     expect(about).toBeInTheDocument()
+
+//     const download = screen.getByAltText("download")
+//     expect(download).toBeInTheDocument()
+
+// })
