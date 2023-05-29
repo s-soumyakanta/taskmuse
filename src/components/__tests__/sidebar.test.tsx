7bdfe1acb4ab1e91f35e08d2b02acd29d4edf817
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2023-05-30 00:34:26 +0530

    sidebar and sidebar test added

---
+++ src/components/__tests__/sidebar.test.tsx
@@ -0,0 +1,16 @@
+import { render,screen } from "@testing-library/react";
+import Sidebar from "../Sidebar";
+
+describe("Sidebar redners correctly",()=>{
+    test("Sidebar svgs renders correctly",()=>{
+        render(<Sidebar />)
+
+        const sidebarElems:string[] = ["task","important","reminder","archive","trash"]
+
+        sidebarElems.forEach((elems:string )=> {
+            const elem:HTMLElement = screen.getByAltText(elems)
+            expect(elem).toBeInTheDocument()
+            
+        });
+    })
+})
